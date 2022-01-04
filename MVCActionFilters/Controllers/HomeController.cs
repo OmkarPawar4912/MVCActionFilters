e8fcfe23f5bd8a8c5322759a83f4baa030a00caa
dev-Omkar <opawar88@gmail.com>
2022-01-04 09:52:29 +0530

    Add project files.

---
+++ MVCActionFilters/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCActionFilters.FilterAttribute;
+using MVCActionFilters.Models;
+using System.Diagnostics;
+
+namespace MVCActionFilters.Controllers
+{
+    [Log]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
